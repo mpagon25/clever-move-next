5ca058f47b9be03f654a90f1354f5b7f5f932fb7
Manuel Pages Gonzales <m.pago92@gmail.com>
2025-05-04 22:55:32 +0200

    feat: add navbar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { montserrat } from "./fonts";
+import NavBar from "./ui/nav-bar/nav-bar";
 
 export const metadata: Metadata = {
   title: "Clever Move",
@@ -14,7 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${montserrat.className} antialiased`}>{children}</body>
+      <body className={`${montserrat.className} antialiased`}>
+        <NavBar />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ app/ui/nav-bar/nav-bar.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <nav className="static flex justify-center h-25 shadow-[2px_-2px_10px_rgba(3,3,3,0.1)]">
+      <div className="flex max-w-[1440px] w-full items-center">
+        <div className="flex flex-2">
+          <p className="text-3xl font-bold ">Clever Move</p>
+        </div>
+
+        <div className="flex flex-3 font-bold justify-around">
+          <div>
+            <p>Home</p>
+          </div>
+          <div>
+            <p>Service</p>
+          </div>
+          <div>
+            <p>About us</p>
+          </div>
+          <div>
+            <p>Contact us</p>
+          </div>
+        </div>
+        <div className="flex flex-2 justify-end font-bold h-[54px]">
+          <p className="h-full flex items-center pr-5 pl-5 mr-4">Login</p>
+          <button className="w-[120px] h-full rounded-full text-white bg-[#2A5F69]">
+            Sign Up
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+}
